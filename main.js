df7829b11e4c09d1a27c86c23d01025a80aab3e0
Jeimo <jjjundante@gmail.com>
2023-07-04 17:10:29 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// Navbar Functionality
+let navbar = document.querySelector('.navbar')
+document.addEventListener('scroll' , () =>{
+    if(window.top.scrollY > 19){
+    }else{
+        navbar.classList.add('scroll');
+        navbar.style.transition = '.2s ease';
+    }
+})
+
+// Slider Functionality
+const productContainers = [...document.querySelectorAll('.company-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+// Toggle Menu Functionality
+let subMenu = document.getElementById("subMenu");
+
+        function toggleMenu(){
+            subMenu.classList.toggle("open-menu");
+        }
+
+// Default theme
+import '@splidejs/splide/css';
+import Splide from '@splidejs/splide';
+
+
+new Splide( '.splide' ).mount();
